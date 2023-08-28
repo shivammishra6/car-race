96d3668e21c34eecd5849974d1950929d6a93f06
shivam <shivammmishra6203@gmail.com>
2023-08-28 16:13:08 +0530

    added music and fixed body width

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const audio=document.createElement('audio')
 audio.setAttribute('src','sound.mp3')
 audio.loop=true
-audio.play()
+play.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play()
+        play.classList.remove('fa-volume-xmark')
+        play.classList.add('fa-volume-high')
+    } else {
+        audio.pause()
+        play.classList.remove('fa-volume-high')
+        play.classList.add('fa-volume-xmark')
+    }
+})
